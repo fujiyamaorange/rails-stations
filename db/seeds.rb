21f069ac17565f3c3b9896784f16c03dab919148
fujiyamaorange <kaito.bakuman@gmail.com>
2022-01-28 08:19:02 +0900

    ✨ station1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.create(name: 'ショーシャンクの空に', year: "2022", description: 'ショーシャンクの空に', image_url: "https://imgc.nxtv.jp/img/info/tit/00003/SID0003844.png?output-format=jpg&output-quality=70&resize=1024:*", is_showing: false, created_at: Date.today, updated_at: Date.today)
+Movie.create(name: 'ショーシャンクの空に', year: "2022", description: 'ショーシャンクの空に', image_url: "https://imgc.nxtv.jp/img/info/tit/00003/SID0003844.png?output-format=jpg&output-quality=70&resize=1024:*", is_showing: false, created_at: Date.today, updated_at: Date.today)
+Movie.create(name: 'ショーシャンクの空に', year: "2022", description: 'ショーシャンクの空に', image_url: "https://imgc.nxtv.jp/img/info/tit/00003/SID0003844.png?output-format=jpg&output-quality=70&resize=1024:*", is_showing: false, created_at: Date.today, updated_at: Date.today)
+Movie.create(name: 'ショーシャンクの空に', year: "2022", description: 'ショーシャンクの空に', image_url: "https://imgc.nxtv.jp/img/info/tit/00003/SID0003844.png?output-format=jpg&output-quality=70&resize=1024:*", is_showing: false, created_at: Date.today, updated_at: Date.today)
+Movie.create(name: 'ショーシャンクの空に', year: "2022", description: 'ショーシャンクの空に', image_url: "https://imgc.nxtv.jp/img/info/tit/00003/SID0003844.png?output-format=jpg&output-quality=70&resize=1024:*", is_showing: false, created_at: Date.today, updated_at: Date.today)
+Movie.create(name: 'ショーシャンクの空に', year: "2022", description: 'ショーシャンクの空に', image_url: "https://imgc.nxtv.jp/img/info/tit/00003/SID0003844.png?output-format=jpg&output-quality=70&resize=1024:*", is_showing: false, created_at: Date.today, updated_at: Date.today)
