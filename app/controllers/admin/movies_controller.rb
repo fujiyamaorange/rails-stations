ca4dd5abec4ec8852baedab88151c0a88e9bfa18
fujiyamaorange <kaito.bakuman@gmail.com>
2022-01-28 14:03:55 +0900

    ✨ station3

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,34 @@ class Admin::MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def new
+  end
+
+  def create
+    # 映画情報の新規作成画面
+    @new_movie = Movie.new(
+      name: params[:title],
+      year: params[:year],
+      is_showing: params[:is_showing].to_i == 1 ? true : false,
+      description: params[:description],
+      image_url: params[:image_url],
+    )
+
+    Rails.logger.debug @new_movie
+
+    begin
+      if @new_movie.save
+        redirect_to("/movies")
+      else
+        flash[:notice] = '登録に失敗しました'
+        Rails.logger.debug "フォームエラー"
+      end
+    rescue => exception
+      flash[:notice] = '登録に失敗しました'
+      Rails.logger.debug "登録に失敗しました"
+    end
+
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
     get 'movies' => 'movies#index'
+    post 'movies' => 'movies#create'
+    get 'movies/new' => 'movies#new'
   end
   get 'movies' => 'movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
