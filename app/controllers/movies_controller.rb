d0ec88dcdd41be4986b8121880626c4f8f1d05b9
fujiyamaorange <kaito.bakuman@gmail.com>
2022-01-28 10:09:45 +0900

    ✨ station2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,5 @@
 class MoviesController < ApplicationController
   def index
-    # モック
-    # mock_movies = Array.new(6, {title: "ショーシャンクの空に", image: "https://imgc.nxtv.jp/img/info/tit/00003/SID0003844.png?output-format=jpg&output-quality=70&resize=1024:*"})
-    # @movies = mock_movies
     @movies = Movie.all
   end
 end
-
-
-movie = Movie.new(name: "ショーシャンクの空に", year: "2022", description: "これはショーシャンクの空にです", image_url: "https://imgc.nxtv.jp/img/info/tit/00003/SID0003844.png?output-format=jpg&output-quality=70&resize=1024:*", is_showing: false, created_at: nil, updated_at: nil)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'movies' => 'movies#index'
+  end
   get 'movies' => 'movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
