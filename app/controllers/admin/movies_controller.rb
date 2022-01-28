a6144608e854621e4c05aebafbfe4d22177fdb11
fujiyamaorange <kaito.bakuman@gmail.com>
2022-01-28 17:43:43 +0900

    ✨ station3

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -4,32 +4,40 @@ class Admin::MoviesController < ApplicationController
   end
 
   def new
+    @movie = Movie.new
   end
 
   def create
     # 映画情報の新規作成画面
-    @new_movie = Movie.new(
-      name: params[:title],
-      year: params[:year],
-      is_showing: params[:is_showing].to_i == 1 ? true : false,
-      description: params[:description],
-      image_url: params[:image_url],
-    )
-
-    Rails.logger.debug @new_movie
+    # @movie = Movie.new(
+    #   name: params[:name],
+    #   year: params[:year],
+    #   is_showing: params[:is_showing].to_i == 1 ? true : false,
+    #   description: params[:description],
+    #   image_url: params[:image_url]
+    # )
+    
+    @movie = Movie.new(movie_params)
+    Rails.logger.debug @movie
 
     begin
-      if @new_movie.save
+      if @movie.save
         redirect_to("/movies")
       else
         flash[:notice] = '登録に失敗しました'
+        redirect_to("/admin/movies/new")
         Rails.logger.debug "フォームエラー"
       end
-    rescue => exception
+    rescue
       flash[:notice] = '登録に失敗しました'
       Rails.logger.debug "登録に失敗しました"
+      redirect_to("/admin/movies/new")
     end
-
-
   end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
+  end
+ 
 end
