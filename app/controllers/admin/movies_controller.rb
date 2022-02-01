eb2282716b45e53c39f8d482fa1dc18467e997f0
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-01 11:12:01 +0900

    ✨ station4

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -9,15 +9,15 @@ class Admin::MoviesController < ApplicationController
 
   def create
     # 映画情報の新規作成画面
-    # @movie = Movie.new(
-    #   name: params[:name],
-    #   year: params[:year],
-    #   is_showing: params[:is_showing].to_i == 1 ? true : false,
-    #   description: params[:description],
-    #   image_url: params[:image_url]
-    # )
-    
-    @movie = Movie.new(movie_params)
+    @movie = Movie.new(
+      name: params[:name],
+      year: params[:year],
+      is_showing: params[:is_showing].to_i == 1 ? true : false,
+      description: params[:description],
+      image_url: params[:image_url]
+    )
+
+    # @movie = Movie.new(movie_params)
     Rails.logger.debug @movie
 
     begin
@@ -35,9 +35,38 @@ class Admin::MoviesController < ApplicationController
     end
   end
 
-  private
-  def movie_params
-    params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
+  def edit
+    @movie = Movie.find_by(id: params[:id]) 
   end
+
+  def update
+    @movie = Movie.find_by(id: params[:id])
+    @movie.name = params[:name]
+    @movie.year = params[:year]
+    @movie.is_showing = params[:is_showing].to_i == 1 ? true : false
+    @movie.description = params[:description]
+    @movie.image_url = params[:image_url]
+
+    begin
+      if @movie.save
+        flash[:notice] = '映画情報を変更しました'
+        redirect_to('/admin/movies')
+      else
+        Rails.logger.debug "400エラー発生"
+        # 400エラーを返す？？
+        redirect_to action: :edit, status: :found
+      end
+    rescue
+      flash[:notice] = '変更に失敗しました'
+      Rails.logger.debug "変更に失敗しました"
+      redirect_to("/admin/movies/#{@move.id}")
+    end
+    
+  end
+  
+  # private
+  # def movie_params
+  #   params.require(:movie).permit(:name, :year, :is_showing, :description, :image_url)
+  # end
  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
     get 'movies' => 'movies#index'
     post 'movies' => 'movies#create'
     get 'movies/new' => 'movies#new'
+    put 'movies/:id' => 'movies#update'
+    get 'movies/:id/edit' => 'movies#edit'
   end
   get 'movies' => 'movies#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
