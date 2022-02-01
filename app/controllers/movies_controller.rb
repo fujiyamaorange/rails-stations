335e27a2dcfc0814ac8061768ae37d941b0bad56
fujiyamaorange <kaito.bakuman@gmail.com>
2022-02-01 17:20:58 +0900

    ✨ station6

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,17 @@
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
+    @query = params[:query]
+    @state = params[:state]
+
+    # ラジオボタンでの絞り
+    selected = Movie.all
+    if params[:state] != 'all'
+      selected = @movies.where(is_showing: params[:state] == 'open' ? true : false) 
+    end
+
+    # キーワードでの絞り
+    @searched = selected.where("name LIKE?", "%#{@query}%").or(selected.where("description LIKE?", "%#{@query}%"))
+
   end
 end
